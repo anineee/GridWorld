b5d7ad5dfaa16f680426448c7eaf6e288c4a3ec5
anineee <49850164+anineee@users.noreply.github.com>
2019-11-09 15:16:10 +0800

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,13 @@
+package Calculator;
+
+import java.util.*;
+
+public class Calculator {
+	public double evalExpression() {
+		double result = 0;
+		Stack<double> operand_s;
+		Stack<Character> oprerator_s;
+		
+		return result;
+	}
+}
